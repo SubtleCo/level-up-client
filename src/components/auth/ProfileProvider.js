bc81a6a29aa6f1f7bd738cb322908ab75183c0fa
Alex Martin <alex@subtleco.co>
2021-05-10 15:55:44 -0500

    added Profile.js and ProfileProvider.js

---
+++ src/components/auth/ProfileProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react'
+
+export const ProfileContext = createContext()
+
+export const ProfileProvider = props => {
+    const [ profile, setProfile ] = useState({events:[]})
+
+    const getProfile = () => {
+        return fetch("http://localhost:8000/profile", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setProfile)
+    }
+
+    return (
+        <ProfileContext.Provider value={{ profile, getProfile }}>
+            {props.children}
+        </ProfileContext.Provider>
+    )
+
+
+}
