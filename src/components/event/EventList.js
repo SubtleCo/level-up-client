a50c3a8139c32a7bc1d2e8a7da2d9f1ef93a2866
Alex Martin <alex@subtleco.co>
2021-05-10 14:01:55 -0500

    User can sign up for events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect } from "react"
-import { useHistory } from "react-router"
 import { EventContext } from "./EventProvider.js"
+import { useHistory } from "react-router-dom"
 
-export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+
+export const EventList = () => {
     const history = useHistory()
+    const { events, getEvents, joinEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -14,29 +15,24 @@ export const EventList = (props) => {
         <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    history.push({ pathname: "/events/new" })
-                }}
-            >Create a new event</button>
             {
                 events.map(event => {
+                    // const attending = profile.events.some(evt => evt.id === event.id)
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
                         <div>
-                            {
-                                new Date(event.date).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                            }
-                            @ {event.time}
+                            {event.date} @ {event.time}
                         </div>
+                        <button className="btn btn-2"
+                                onClick={() => joinEvent(event.id)}
+                        >Join</button>
                     </section>
                 })
             }
